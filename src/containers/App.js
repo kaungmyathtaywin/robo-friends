c958b2044b75240830e65572a1721e0e21be42b6
Kaung Myat Htay Win <kaungmyathtaywin@Kaungs-MacBook-Pro.local>
2021-03-26 00:38:40 +0630

    Deployment bug fixes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 class App extends React.Component {
@@ -18,7 +19,7 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    fetch("http://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ robots: users }));
   }
@@ -35,10 +36,12 @@ class App extends React.Component {
     } else {
       return (
         <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
+          <h1 className="f2s">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
